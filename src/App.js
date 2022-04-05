98f9035648c7e6ff9239afebfc7258c89c3333de
jahid4striver <jahidhassn701@gmail.comr>
2022-04-05 19:49:32 +0600

    Show all added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
+import Reviews from './components/Reviews/Reviews';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/reviews' element={<Reviews/>}></Route>
       </Routes>
     </div>
   );
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useReviews from '../../hooks/useReviews';
 import Review from '../Review/Review';
 
@@ -8,11 +9,12 @@ const Reviews = () => {
     return (
         <div>
             <h1>Reviews Of Our Honorable Customers</h1>
-            <div className='row mt-5'>
+            <div className='container row mt-5'>
             {
                 reviews.map(review=><Review review={review} key={review.id}/>)
             }
             </div>
+            <Link to='/reviews'><button className='btn btn-lg btn-warning fw-bold'>Show All Reviews</button></Link>
         </div>
     );
 };
